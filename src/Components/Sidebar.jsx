4e967a84bf4e3af321d2944f467e93d58085df38
Kenny Hector <kennyhector24@gmail.com>
2024-10-21 18:36:22 -0400

    Update sidebar to be sticky

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,13 +4,15 @@ import '../Styles/Components-Styles/Sidebar.css';
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <h3>Dashboard</h3>
-            <nav>
-                <NavLink to="/" >Home</NavLink>
-                <NavLink to="/deliveries" >Deliveries</NavLink>
-                <NavLink to="/managers" >Managers</NavLink>
-                <NavLink to="/drivers">Drivers</NavLink>
-            </nav>
+            <div className='dashboard'>
+                <h3>Dashboard</h3>
+                <nav>
+                    <NavLink to="/" >Home</NavLink>
+                    <NavLink to="/deliveries" >Deliveries</NavLink>
+                    <NavLink to="/managers" >Managers</NavLink>
+                    <NavLink to="/drivers">Drivers</NavLink>
+                </nav>
+            </div>
         </div>
     );
 }
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -8,8 +8,10 @@ import Header from './Components/Header';
 const Layout = () => {
     return (
         <div style={{ display: 'flex', height: '100vh' }}>
-            <Sidebar />
-            <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+            <div style={{ flex: .25 }} >
+                <Sidebar />
+            </div>
+            <div style={{ flex: .75, display: 'flex', flexDirection: 'column' }}>
                 <Header />
                 <div style={{ flex: 1, padding: 20 }}>
                     <Outlet />
